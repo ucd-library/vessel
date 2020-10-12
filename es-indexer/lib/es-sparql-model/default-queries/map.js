8f109d081770e9c84ef23ce9a11084aa05075119
Quinn Hart <qjhart@ucdavis.edu>
2020-10-12 00:30:36 -0700

    Updated map for multiple publication types.

--- es-indexer/lib/es-sparql-model/default-queries/map.js
+++ es-indexer/lib/es-sparql-model/default-queries/map.js
@@ -3,7 +3,7 @@ module.exports = {
   person : ['http://xmlns.com/foaf/0.1/Person', 'http://vivoweb.org/ontology/core#NonAcademic','http://vivoweb.org/ontology/core#FacultyMember'],
 
   // https://wiki.lyrasis.org/display/VIVODOC110x/Publication+Model
-  publication : 'http://purl.org/ontology/bibo/AcademicArticle',
+  publication : ['http://purl.org/ontology/bibo/AcademicArticle','http://purl.org/ontology/bibo/Book','http://purl.org/ontology/bibo/Chapter','http://vivoweb.org/ontology/core#ConferencePaper'],
 
   organization : ['http://vivoweb.org/ontology/core#AcademicDepartment', 'http://vivoweb.org/ontology/core#College','http://vivoweb.org/ontology/core#University']
 }
