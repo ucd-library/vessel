22c1e3047f53fd4dfa2d6d6415e04426629e7354
Justin Merz <jrmerz@gmail.com>
2021-07-22 16:00:02 -0700

    setting origin for cors requests

--- gateway/index.js
+++ gateway/index.js
@@ -5,6 +5,7 @@ const compression = require('compression');
 const httpProxy = require('http-proxy');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')({
+  origin: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   exposedHeaders : ['content-type', 'link', 'content-disposition', 'content-length', 'pragma', 'expires', 'cache-control'],
   allowedHeaders : ['authorization', 'range', 'cookie', 'content-type', 'prefer', 'slug', 'cache-control', 'accept'],
