6c20bfae326d0518c5e53b4ffcd446ec193bf3ca
Quinn Hart <qjhart@ucdavis.edu>
2022-05-05 16:51:08 -0700

    Updated concept types for index.

--- es-models/default/queries/map.js
+++ es-models/default/queries/map.js
@@ -14,7 +14,7 @@ module.exports = {
   },
 
   concept : [
-//    'http://www.w3.org/2004/02/skos/core#Concept',
+    'http://www.w3.org/2004/02/skos/core#Concept',
     'http://experts.ucdavis.edu/schema#concept'
             ],
 
